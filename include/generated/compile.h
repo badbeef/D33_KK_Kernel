fbf63ce4178b07dfa0774ed085112ec0f48c1339
badbeef <badbeef@github.com>
2014-11-04 13:51:45 -0800

    Patched in .config with touch support

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 17 */
+/* This file is auto generated, version 18 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#17 SMP PREEMPT Tue May 6 07:06:52 BST 2014"
-#define LINUX_COMPILE_BY "darrell"
-#define LINUX_COMPILE_HOST "Gargleblaster"
-#define LINUX_COMPILER "gcc version 4.8.2 (Ubuntu/Linaro 4.8.2-16ubuntu4) "
+#define UTS_VERSION "#18 SMP PREEMPT Tue Nov 4 13:49:21 PST 2014"
+#define LINUX_COMPILE_BY "mongkok"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.6.3 (Ubuntu/Linaro 4.6.3-1ubuntu5) "
